ffce994c0f6a8f125055f5533d2614785ecc2641
Соня <lisofang@yandex.ru>
2022-12-08 23:06:28 +0300

    red_1

--- Home_work/Home_work_05.py
+++ Home_work/Home_work_05.py
@@ -4,6 +4,23 @@
 #  Пугать ты галок пугай => заданная строка "пуг" => Пугать ты галок
 
 
+# test_str = input('Введите текст: ')
+# find_str = input('Введите заданную строку: ')
+test_str = 'Пугать ты галок пугай'
+find_str = 'галок'
+
+with open('textIN_home_work_5.txt','w') as file:
+        file.write(test_str)
+
+with open('textIN_home_work_5.txt') as infile, open('textOUT.txt', "w") as outfile:
+    for line in infile:
+        line = line.split()
+        for i in range(len(line)):
+               if line[i].find(find_str) == -1:
+                    outfile.write(line[i] + " ")
+               
+
+
 # ; 2.Создайте программу для игры с конфетами человек против человека.
 # ; Условие задачи: На столе лежит 2021(или сколько вы скажете) конфета.
 # ;  Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. 
@@ -13,6 +30,44 @@
 # ; b) Подумайте как наделить бота ""интеллектом""
 # ; Если делаете a и b - не нужно создавать отдельных файлов
 # ;  с полностью копированным кодом, лучше выделите в отдельные функции бота и умного бота.
+
+
+def Сreate_random_list(len_list: int,first_num: int, second_num: int) -> (list):
+     """функция задает список из случайных чисел"""
+     from random import randint
+     newlist = []
+     for i in range(len_list):
+         newlist.append(randint(first_num, second_num))
+     return newlist
+
+# bonbon = int(input("Введите количество конфет: "))
+
+# max_one_move = int(input("Введите максимальное количество конфет за один ход обоих игроков: "))
+# print(f"Подсказка: сколько конфет взять в первый ход, чтобы победить: {bonbon % (max_one_move + 1)}")
+
+# response = input("Хотите узнать, какой вы игрок? Нажмите enter")
+# lot = Сreate_random_list(1,0,2)
+# if lot[0] == 1:
+#     print("Вы первый игрок!")
+# else:
+#     print("Вы второй игрок!")
+
+
+# while bonbon >= 1:
+#     move = int(input(f"Первый игрок, введите количество конфет от 1 до {max_one_move}: "))
+#     bonbon = bonbon - move
+#     print(f"Количество оставшихся конфет: {bonbon}")
+#     if bonbon == 1:
+#         print("Победитель - первый игрок")
+#         break
+    
+#     move = int(input(f"Второй игрок, введите количество конфет от 1 до {max_one_move}: "))
+#     bonbon = bonbon - move
+#     print(f"Количество оставшихся конфет: {bonbon}")
+#     if bonbon == 1:
+#         print("Победитель - второй игрок")
+#         break
+
 
 
 
