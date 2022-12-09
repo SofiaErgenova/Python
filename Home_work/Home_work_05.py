fcb4ba3ac2c4fc4ed335673140dfbbc11ce43fb5
Соня <lisofang@yandex.ru>
2022-12-09 11:13:11 +0300

    pluszero

--- Home_work/Home_work_05.py
+++ Home_work/Home_work_05.py
@@ -6,18 +6,18 @@
 
 # test_str = input('Введите текст: ')
 # find_str = input('Введите заданную строку: ')
-test_str = 'Пугать ты галок пугай'
-find_str = 'галок'
+# test_str = 'Пугать ты галок пугай'
+# find_str = 'галок'
 
-with open('textIN_home_work_5.txt','w') as file:
-        file.write(test_str)
+# with open('textIN_home_work_5.txt','w') as file:
+#         file.write(test_str)
 
-with open('textIN_home_work_5.txt') as infile, open('textOUT.txt', "w") as outfile:
-    for line in infile:
-        line = line.split()
-        for i in range(len(line)):
-               if line[i].find(find_str) == -1:
-                    outfile.write(line[i] + " ")
+# with open('textIN_home_work_5.txt') as infile, open('textOUT.txt', "w") as outfile:
+#     for line in infile:
+#         line = line.split()
+#         for i in range(len(line)):
+#                if line[i].find(find_str) == -1:
+#                     outfile.write(line[i] + " ")
                
 
 
@@ -72,3 +72,138 @@ def Сreate_random_list(len_list: int,first_num: int, second_num: int) -> (list)
 
 
 #  3.Создайте программу для игры в ""Крестики-нолики"".
+
+row_one = ['*' ,'*', '*']
+row_two = ['*' ,'*', '*']
+row_three = ['*' ,'*', '*']
+
+print (''. join(row_one))
+print(''. join(row_two))
+print(''. join(row_three))
+
+
+response = input("Кто ходит первым? Нажмите enter")
+lot = Сreate_random_list(1,0,2)
+if lot[0] == 1:
+    print("Крестики!")
+else:
+    print("Нолики!")
+
+count = 0 
+while count <= 9:
+        if lot[0] == 0:
+                row = int(input("Ход ноликов.Введите номер ряда 1, 2 или 3: "))
+                i = int(input("Введите номер позиции 0, 1 или 2: "))
+                if row == 1 and lot[0] == 0:
+                        row_one[i] = '0'
+                        print (''. join(row_one))
+                        print(''. join(row_two))
+                        print(''. join(row_three))
+                        
+                        if (row_one[0] == row_two[1]== row_three[2]== '0' or
+                        row_one[2] == row_two[1]== row_three[0]== '0' or
+                        row_one[0] == row_two[0]== row_three[0]== '0' or
+                        row_one[1] == row_two[1]== row_three[1]== '0' or
+                        row_one[2] == row_two[2]== row_three[2]== '0' or
+                        row_one[0] == row_one[1]== row_one[2]== '0' or
+                        row_two[0] == row_two[1]== row_two[2]== '0' or
+                        row_three[0] == row_three[1]== row_three[2]== '0'):
+                                print("Победили нолики!")
+                                break
+                        count += 1
+                        lot[0] == 1
+                elif row == 2  and lot[0] == 0:
+                        row_two[i] = '0'
+                        print (''. join(row_one))
+                        print(''. join(row_two))
+                        print(''. join(row_three))
+                        if (row_one[0] == row_two[1]== row_three[2]== '0' or
+                        row_one[2] == row_two[1]== row_three[0]== '0' or
+                        row_one[0] == row_two[0]== row_three[0]== '0' or
+                        row_one[1] == row_two[1]== row_three[1]== '0' or
+                        row_one[2] == row_two[2]== row_three[2]== '0' or
+                        row_one[0] == row_one[1]== row_one[2]== '0' or
+                        row_two[0] == row_two[1]== row_two[2]== '0' or
+                        row_three[0] == row_three[1]== row_three[2]== '0'):
+                                print("Победили нолики!")
+                                break
+                        count += 1
+                        lot[0] == 1
+                elif row == 3 and lot[0] == 0:
+                        row_three[i] = '0'
+                        print (''. join(row_one))
+                        print(''. join(row_two))
+                        print(''. join(row_three))
+                        if (row_one[0] == row_two[1]== row_three[2]== '0' or
+                        row_one[2] == row_two[1]== row_three[0]== '0' or
+                        row_one[0] == row_two[0]== row_three[0]== '0' or
+                        row_one[1] == row_two[1]== row_three[1]== '0' or
+                        row_one[2] == row_two[2]== row_three[2]== '0' or
+                        row_one[0] == row_one[1]== row_one[2]== '0' or
+                        row_two[0] == row_two[1]== row_two[2]== '0' or
+                        row_three[0] == row_three[1]== row_three[2]== '0'):
+                                print("Победили нолики!")
+                                break
+                        count += 1
+                        lot[0] == 1
+                else:
+                        print("Неверное значение")
+        elif lot[0] == 1:
+                row = int(input("Ход крестиков.Введите номер ряда 1, 2 или 3: "))
+                i = int(input("Введите номер позиции 0, 1 или 2: "))
+                if row == 1  and lot[0] == 1:
+                        row_one[i] = '+'
+                        print (''. join(row_one))
+                        print(''. join(row_two))
+                        print(''. join(row_three))
+                        if (row_one[0] == row_two[1]== row_three[2]== '+' or
+                        row_one[2] == row_two[1]== row_three[0]== '+' or
+                        row_one[0] == row_two[0]== row_three[0]== '+' or
+                        row_one[1] == row_two[1]== row_three[1]== '+' or
+                        row_one[2] == row_two[2]== row_three[2]== '+' or
+                        row_one[0] == row_one[1]== row_one[2]== '+' or
+                        row_two[0] == row_two[1]== row_two[2]== '+' or
+                        row_three[0] == row_three[1]== row_three[2]== '+'):
+                                print("Победили крестики!")
+                                break
+                        count += 1
+                        lot[0] == 0
+                elif row == 2 and lot[0] == 1:
+                        row_two[i] = '+'
+                        print (''. join(row_one))
+                        print(''. join(row_two))
+                        print(''. join(row_three))
+                        if (row_one[0] == row_two[1]== row_three[2]== '+' or
+                        row_one[2] == row_two[1]== row_three[0]== '+' or
+                        row_one[0] == row_two[0]== row_three[0]== '+' or
+                        row_one[1] == row_two[1]== row_three[1]== '+' or
+                        row_one[2] == row_two[2]== row_three[2]== '+' or
+                        row_one[0] == row_one[1]== row_one[2]== '+' or
+                        row_two[0] == row_two[1]== row_two[2]== '+' or
+                        row_three[0] == row_three[1]== row_three[2]== '+'):
+                                print("Победили крестики!")
+                                break
+                        count += 1
+                        lot[0] == 0
+                elif row == 3 and lot[0] == 1:
+                        row_three[i] = '+'
+                        print (''. join(row_one))
+                        print(''. join(row_two))
+                        print(''. join(row_three))
+                        if (row_one[0] == row_two[1]== row_three[2]== '+' or
+                        row_one[2] == row_two[1]== row_three[0]== '+' or
+                        row_one[0] == row_two[0]== row_three[0]== '+' or
+                        row_one[1] == row_two[1]== row_three[1]== '+' or
+                        row_one[2] == row_two[2]== row_three[2]== '+' or
+                        row_one[0] == row_one[1]== row_one[2]== '+' or
+                        row_two[0] == row_two[1]== row_two[2]== '+' or
+                        row_three[0] == row_three[1]== row_three[2]== '+'):
+                                print("Победили крестики!")
+                                break
+                        count += 1
+                        lot[0] == 0
+                else:
+                        print("Неверное значение")
+else:
+        print("Ничья")
+        
